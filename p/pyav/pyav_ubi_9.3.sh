9909a1e32516653b4f4ad638cee89e885525d794
aastha-sharma2 <aastha.sharma4@ibm.com>
2025-08-19 15:46:46 +0530

    Create pyav_ubi_9.3.sh

---
+++ p/pyav/pyav_ubi_9.3.sh
@@ -0,0 +1,280 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package       : PyAV
+# Version       : v14.4.0
+# Source repo   : https://github.com/PyAV-Org/PyAV
+# Tested on     : UBI 9.3
+# Language      : Python
+# Travis-Check  : True
+# Script License: Apache License 2.0
+# Maintainer    : Aastha Sharma <aastha.sharma4@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+set -ex
+
+PACKAGE_NAME=PyAV
+PACKAGE_VERSION=${1:-v14.4.0}
+PACKAGE_URL=https://github.com/PyAV-Org/PyAV
+CURRENT_DIR=$(pwd)
+PACKAGE_DIR=PyAV
+
+# install core dependencies
+yum install -y wget python python-pip python-devel gcc-toolset-13 gcc-toolset-13-binutils-devel gcc-toolset-13-gcc-c++ git make cmake binutils libjpeg-turbo-devel autoconf automake libtool gmp-devel freetype-devel openssl-devel
+
+export PATH=/opt/rh/gcc-toolset-13/root/usr/bin:$PATH
+export LD_LIBRARY_PATH=/opt/rh/gcc-toolset-13/root/usr/lib64:$LD_LIBRARY_PATH
+gcc --version
+
+export GCC_HOME=/opt/rh/gcc-toolset-13/root/usr
+export CC=$GCC_HOME/bin/gcc
+export CXX=$GCC_HOME/bin/g++
+
+
+OS_NAME=$(cat /etc/os-release | grep ^PRETTY_NAME | cut -d= -f2)
+
+python -m pip install --upgrade pip
+
+INSTALL_ROOT="/install-deps"
+mkdir -p $INSTALL_ROOT
+
+
+for package in lame opus libvpx ffmpeg pillow numpy; do
+    mkdir -p ${INSTALL_ROOT}/${package}
+    export "${package^^}_PREFIX=${INSTALL_ROOT}/${package}" # convert package name to upper code ${package^^}
+    echo "Exported ${package^^}_PREFIX=${INSTALL_ROOT}/${package}"
+done
+
+python -m pip install cython pytest
+
+echo "Installing NumPy"
+
+python -m pip install numpy==2.0.2
+
+#install yasm
+cd $CURRENT_DIR
+curl -LO https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure
+make
+make install
+
+#installing libvpx
+cd $CURRENT_DIR
+git clone https://github.com/webmproject/libvpx.git
+cd libvpx
+git checkout v1.13.1
+export target_platform=$(uname)-$(uname -m)
+if [[ ${target_platform} == Linux-* ]]; then
+    LDFLAGS="$LDFLAGS -pthread"
+fi
+CPU_DETECT="${CPU_DETECT} --enable-runtime-cpu-detect"
+
+./configure --prefix=$LIBVPX_PREFIX \
+--as=yasm                    \
+--enable-shared              \
+--disable-static             \
+--disable-install-docs       \
+--disable-install-srcs       \
+--enable-vp8                 \
+--enable-postproc            \
+--enable-vp9                 \
+--enable-vp9-highbitdepth    \
+--enable-pic                 \
+${CPU_DETECT}                \
+--enable-experimental || { cat config.log; exit 1; }
+
+make
+make install PREFIX="${LIBVPX_PREFIX}"
+export LD_LIBRARY_PATH=${LIBVPX_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${LIBVPX_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+pkg-config --modversion vpx
+echo "-----------------------------------------------------Installed libvpx------------------------------------------------"
+
+
+#installing lame
+cd $CURRENT_DIR
+wget https://downloads.sourceforge.net/sourceforge/lame/lame-3.100.tar.gz
+tar -xvf lame-3.100.tar.gz
+cd lame-3.100
+# remove libtool files
+find $LAME_PREFIX -name '*.la' -delete
+
+./configure --prefix=$LAME_PREFIX \
+            --disable-dependency-tracking \
+            --disable-debug \
+            --enable-shared \
+            --enable-static \
+            --enable-nasm
+
+make
+make install PREFIX="${LAME_PREFIX}"
+export LD_LIBRARY_PATH=/install-deps/lame/lib:$LD_LIBRARY_PATH
+export PATH="/install-deps/lame/bin:$PATH"
+lame --version
+echo "-----------------------------------------------------Installed lame------------------------------------------------"
+
+
+#installing opus
+cd $CURRENT_DIR
+git clone https://github.com/xiph/opus
+cd opus
+git checkout v1.3.1
+./autogen.sh
+./configure --prefix=$OPUS_PREFIX
+make
+make install PREFIX="${OPUS_PREFIX}"
+export LD_LIBRARY_PATH=${OPUS_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${OPUS_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+pkg-config --modversion opus
+echo "-----------------------------------------------------Installed opus------------------------------------------------"
+
+
+#installing ffmpeg
+cd $CURRENT_DIR
+git clone https://github.com/FFmpeg/FFmpeg
+cd FFmpeg
+git checkout n7.1
+git submodule update --init
+
+export CPU_COUNT=$(nproc)
+
+USE_NONFREE=no   #the options below are set for NO
+./configure \
+        --prefix="$FFMPEG_PREFIX" \
+        --cc=${CC} \
+        --disable-doc \
+        --enable-gmp \
+        --enable-hardcoded-tables \
+        --enable-libfreetype \
+        --enable-pthreads \
+        --enable-postproc \
+        --enable-pic \
+        --enable-pthreads \
+        --enable-shared \
+        --enable-static \
+        --enable-version3 \
+        --enable-zlib \
+        --enable-libopus \
+        --enable-libmp3lame \
+        --enable-libvpx \
+        --extra-cflags="-I$LAME_PREFIX/include -I$OPUS_PREFIX/include -I$LIBVPX_PREFIX/include" \
+        --extra-ldflags="-L$LAME_PREFIX/lib -L$OPUS_PREFIX/lib -L$LIBVPX_PREFIX/lib" \
+        --disable-encoder=h264 \
+        --disable-decoder=h264 \
+        --disable-decoder=libh264 \
+        --disable-decoder=libx264 \
+        --disable-decoder=libopenh264 \
+        --disable-encoder=libopenh264 \
+        --disable-encoder=libx264 \
+        --disable-decoder=libx264rgb \
+        --disable-encoder=libx264rgb \
+        --disable-encoder=hevc \
+        --disable-decoder=hevc \
+        --disable-encoder=aac \
+        --disable-decoder=aac \
+        --disable-decoder=aac_fixed \
+        --disable-encoder=aac_latm \
+        --disable-decoder=aac_latm \
+        --disable-encoder=mpeg \
+        --disable-encoder=mpeg1video \
+        --disable-encoder=mpeg2video \
+        --disable-encoder=mpeg4 \
+        --disable-encoder=msmpeg4 \
+        --disable-encoder=mpeg4_v4l2m2m \
+        --disable-encoder=msmpeg4v2 \
+        --disable-encoder=msmpeg4v3 \
+        --disable-decoder=mpeg \
+        --disable-decoder=mpegvideo \
+        --disable-decoder=mpeg1video \
+        --disable-decoder=mpeg1_v4l2m2m \
+        --disable-decoder=mpeg2video \
+        --disable-decoder=mpeg2_v4l2m2m \
+        --disable-decoder=mpeg4 \
+        --disable-decoder=msmpeg4 \
+        --disable-decoder=mpeg4_v4l2m2m \
+        --disable-decoder=msmpeg4v1 \
+        --disable-decoder=msmpeg4v2 \
+        --disable-decoder=msmpeg4v3 \
+        --disable-encoder=h264_v4l2m2m \
+        --disable-decoder=h264_v4l2m2m \
+        --disable-encoder=hevc_v4l2m2m \
+        --disable-decoder=hevc_v4l2m2m \
+        --disable-nonfree --disable-gpl --disable-gnutls --enable-openssl --disable-libopenh264 --disable-libx264    #"${_CONFIG_OPTS[@]}"
+
+make -j$CPU_COUNT
+make install PREFIX="${FFMPEG_PREFIX}"
+export PKG_CONFIG_PATH=${FFMPEG_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${FFMPEG_PREFIX}/lib:${LD_LIBRARY_PATH}
+export PATH="/install-deps/ffmpeg/bin:$PATH"
+ffmpeg -version
+echo "-----------------------------------------------------Installed ffmpeg------------------------------------------------"
+
+#installing pillow
+cd $CURRENT_DIR
+git clone https://github.com/python-pillow/Pillow
+cd Pillow
+git checkout 11.1.0
+git submodule update --init
+
+python -m pip install .
+
+echo "-----------------------------------------------------Installed pillow------------------------------------------------"
+
+# clone source repository
+cd $CURRENT_DIR
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+git submodule update --init
+
+export CFLAGS="${CFLAGS} -I/install-deps/ffmpeg/include"
+export LDFLAGS="${LDFLAGS} -L/install-deps/ffmpeg/lib"
+
+# Fix license field in pyproject.toml to comply with PEP 621
+sed -i 's/^license = "BSD-3-Clause"/license = { text = "BSD-3-Clause" }/' pyproject.toml
+
+# Set the paths where your custom FFmpeg and codecs (libvpx, lame, opus) are installed
+export LD_LIBRARY_PATH=/install-deps/ffmpeg/lib:/install-deps/lame/lib:/install-deps/opus/lib:/install-deps/libvpx/lib:$LD_LIBRARY_PATH
+# Persist these paths for the system linker
+echo "/install-deps/ffmpeg/lib" > /etc/ld.so.conf.d/ffmpeg.conf
+echo "/install-deps/lame/lib"   >> /etc/ld.so.conf.d/ffmpeg.conf
+echo "/install-deps/opus/lib"   >> /etc/ld.so.conf.d/ffmpeg.conf
+echo "/install-deps/libvpx/lib" >> /etc/ld.so.conf.d/ffmpeg.conf
+ldconfig
+
+#Build package
+if ! (python setup.py build_ext --inplace) ; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#During wheel creation for this package we need installed dependencies from source. Once script get exit, and if we build wheel through wrapper script, then those are not applicable during wheel creation. So we are generating wheel for this package in script itself.
+echo "---------------------------------------------------Building the wheel--------------------------------------------------"
+pip install --upgrade pip build setuptools wheel
+python -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"
+
+echo "----------------------------------------------Testing pkg-------------------------------------------------------"
+#Test package
+#Skipping few tests as they are failing because of disabling some codecs related to audio and video in ffmpeg. We disabled those codecs because of license associated with them.
+if ! ( pytest --ignore=tests/test_timeout.py  --ignore=tests/test_audiofifo.py   --ignore=tests/test_bitstream.py   --ignore=tests/test_codec_context.py   --ignore=tests/test_colorspace.py   --ignore=tests/test_decode.py   --ignore=tests/test_encode.py   --ignore=tests/test_open.py   --ignore=tests/test_packet.py   --ignore=tests/test_python_io.py   --ignore=tests/test_seek.py   --ignore=tests/test_streams.py   --ignore=tests/test_subtitles.py   --ignore=tests/test_videoframe.py   -k "not test_qmin_qmax and not test_profiles and not test_printing_video_stream and not test_frame_duration_matches_packet and not test_printing_video_stream2 and not test_no_side_data and not test_encoding_dnxhd and not test_encoding_dvvideo and not test_encoding_h264 and not test_encoding_mjpeg and not test_encoding_mpeg1video and not test_encoding_mpeg4 and not test_encoding_png and not test_encoding_tiff and not test_encoding_xvid and not test_mov and not test_decode_video_corrupt and not test_decoded_motion_vectors and not test_decoded_motion_vectors_no_flag and not test_decoded_time_base and not test_decoded_video_frame_count and not test_flush_decoded_video_frame_count and not test_av_stream_Stream and not test_encoding_with_pts and not test_stream_audio_resample and not test_max_b_frames and not test_container_probing and not test_stream_probing and not test_writing_to_custom_io_dash and not test_decode_half and not test_stream_seek and not test_side_data and not test_opaque and not test_reformat_pixel_format_align and not test_filter_output_parameters and not test_codec_mpeg4_decoder and not test_codec_mpeg4_encoder and not test_codec_delay and not test_codec_tag and not test_decoder_extradata and not test_decoder_gop_size and not test_decoder_timebase and not test_encoder_extradata and not test_encoder_pix_fmt and not test_parse and not test_sky_timelapse and not test_av_codec_codec_Codec and not test_av_enum_EnumFlag and not test_av_enum_EnumItem and not test_default_options and not test_encoding and not test_encoding_with_unicode_filename and not test_stream_index and not test_writing_to_buffer and not test_writing_to_buffer_broken and not test_writing_to_buffer_broken_with_close and not test_writing_to_file and not test_writing_to_pipe_writeonly") ; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
