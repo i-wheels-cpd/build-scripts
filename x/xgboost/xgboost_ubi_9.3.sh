e01f877595e8dd7ae0489ee6a71c3af72800f3ec
Aastha Sharma <aastha.sharma4@ibm.com>
2025-09-04 23:46:56 +0530

    Update xgboost_ubi_9.3.sh

--- x/xgboost/xgboost_ubi_9.3.sh
+++ x/xgboost/xgboost_ubi_9.3.sh
@@ -123,7 +123,7 @@ echo "SRC_DIR: $SRC_DIR"
 # Apply the patch
 echo "------------------------Applying patch-------------------"
 wget https://raw.githubusercontent.com/i-wheels-cpd/build-scripts/refs/heads/main/x/xgboost/0001-renaming-the-package-name.patch
-sed -n '/^diff --git/,$p' 0001-renaming-the-package-name.patch | \
+sed -n '/^diff --git /,/^$/p' 0001-renaming-the-package-name.patch | \
   sed '/^@@ -7,7 +7,7 @@/,/^@@/d' | \
   sed 's/\r$//' | \
   git apply
