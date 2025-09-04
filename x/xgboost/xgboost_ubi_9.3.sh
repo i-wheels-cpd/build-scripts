d3ab43a02bcf965bfabaa7ea1c208dfc76067f45
Aastha Sharma <aastha.sharma4@ibm.com>
2025-09-05 00:52:31 +0530

    Update xgboost_ubi_9.3.sh

--- x/xgboost/xgboost_ubi_9.3.sh
+++ x/xgboost/xgboost_ubi_9.3.sh
@@ -124,7 +124,7 @@ echo "SRC_DIR: $SRC_DIR"
 echo "------------------------Applying patch-------------------"
 wget https://raw.githubusercontent.com/i-wheels-cpd/build-scripts/refs/heads/main/x/xgboost/0001-renaming-the-package-name.patch
 sed -n '/^diff --git /,$p' 0001-renaming-the-package-name.patch | \
-  sed '/^@@ -7,7 +7,7 @@ backend-path = \["."\]/,/^@@/d' | \
+  sed '/^@@ -7,7 +7,7 @@ backend-path = \["."\]/,/^+.*\[tool\.hatch\.build\.targets\.wheel\]/d' | \
   sed 's/\r$//' | \
   grep -v '^From ' | \
   git apply
