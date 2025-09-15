be99ceef3e64eacd6025d7ba7da74d46667501bb
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-09-15 12:51:10 +0530

    Update tensorflow-datasets_ubi_9.3.sh

--- t/tensorflow-datasets/tensorflow-datasets_ubi_9.3.sh
+++ t/tensorflow-datasets/tensorflow-datasets_ubi_9.3.sh
@@ -4,7 +4,7 @@
 # Package          : tensorflow-datasets
 # Version          : v4.9.7
 # Source repo      : https://github.com/tensorflow/datasets
-# Tested on        : UBI:9.3
+# Tested on        : UBI:9.6
 # Language         : Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
