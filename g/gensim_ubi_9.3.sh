0a3db8feddd37e48b49d1c8a690600d3b0b183bd
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-09-15 09:41:00 +0530

    Update gensim_ubi_9.3.sh

--- g/gensim_ubi_9.3.sh
+++ g/gensim_ubi_9.3.sh
@@ -4,7 +4,7 @@
 # Package          : gensim
 # Version          : 4.3.3
 # Source repo      : https://github.com/piskvorky/gensim
-# Tested on        : UBI:9.3
+# Tested on        : UBI:9.6
 # Language         : Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
