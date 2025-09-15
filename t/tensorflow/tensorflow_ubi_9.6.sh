af1110c85665b74b5ebbcc11943734192122a387
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-09-15 12:54:17 +0530

    Update and rename tensorflow_ubi_9.3.sh to tensorflow_ubi_9.6.sh

--- t/tensorflow/tensorflow_ubi_9.6.sh
+++ t/tensorflow/tensorflow_ubi_9.6.sh
@@ -4,7 +4,7 @@
 # Package          : tensorflow
 # Version          : v2.18.1
 # Source repo      : https://github.com/tensorflow/tensorflow
-# Tested on        : UBI:9.3
+# Tested on        : UBI:9.6
 # Language         : Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
