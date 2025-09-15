39b61d257c741e2e08e18662c70eaaae5a03b1d8
Aastha Sharma <aastha.sharma4@ibm.com>
2025-09-15 18:18:12 +0530

    Update and rename torchaudio_ubi_9.3.sh to torchaudio_ubi_9.6.sh

--- t/torchaudio/torchaudio_ubi_9.6.sh
+++ t/torchaudio/torchaudio_ubi_9.6.sh
@@ -4,7 +4,7 @@
 # Package          : torchaudio
 # Version          : v2.6.0
 # Source repo      : https://github.com/pytorch/audio.git
-# Tested on        : UBI:9.3
+# Tested on        : UBI:9.6
 # Language         : Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
