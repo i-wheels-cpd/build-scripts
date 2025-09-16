816411176dfe41d16a09b564945743dabd4acd2e
Aastha Sharma <aastha.sharma4@ibm.com>
2025-09-16 13:33:51 +0530

    OSE: Updated scripts for ubi from 9.3 to 9.6 (#58)
    * Update build_info.json
    
    * Update and rename opus_ubi_9.3.sh to opus_ubi_9.6.sh
    
    * Update build_info.json
    
    * Update pyav_ubi_9.3.sh
    
    * Update build_info.json
    
    * Update and rename ffmpeg_ubi_9.3.sh to ffmpeg_ubi_9.6.sh
    
    * Update build_info.json
    
    * Update and rename torchaudio_ubi_9.3.sh to torchaudio_ubi_9.6.sh
    
    * Update build_info.json
    
    * Update and rename libvpx_ubi_9.3.sh to libvpx_ubi_9.6.sh
    
    * Rename pyav_ubi_9.3.sh to pyav_ubi_9.6.sh

--- l/libvpx/libvpx_ubi_9.6.sh
+++ l/libvpx/libvpx_ubi_9.6.sh
@@ -4,7 +4,7 @@
 # Package       : libvpx
 # Version       : 1.3.1
 # Source repo   : https://github.com/webmproject/libvpx
-# Tested on     : UBI:9.3
+# Tested on     : UBI:9.6
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
