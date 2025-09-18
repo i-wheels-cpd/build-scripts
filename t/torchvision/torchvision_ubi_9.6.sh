e6bbddd6260a1a278f6242c49362b55c96875d4e
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2025-09-18 17:37:07 +0530

    Update torchvision_ubi_9.6.sh

--- t/torchvision/torchvision_ubi_9.6.sh
+++ t/torchvision/torchvision_ubi_9.6.sh
@@ -28,6 +28,7 @@ CURRENT_DIR=${PWD}
 
 yum install -y git make cmake zip tar wget python3.12 python3.12-devel python3.12-pip gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc zlib-devel libjpeg-devel openssl openssl-devel freetype-devel pkgconfig ninja-build sqlite-devel rsync java-11-openjdk java-11-openjdk-devel zlib-devel freetype-devel pkgconfig
 
+
 python3.12 -m pip install setuptools wheel build pytest
 
 #Increment by 1 for each build
