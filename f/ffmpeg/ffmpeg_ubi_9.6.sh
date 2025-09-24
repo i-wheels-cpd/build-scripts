f202b194e71588e4a444952460fcc0f71cd6bbb8
Aastha Sharma <aastha.sharma4@ibm.com>
2025-09-24 14:32:51 +0530

    Update ffmpeg_ubi_9.6.sh

--- f/ffmpeg/ffmpeg_ubi_9.6.sh
+++ f/ffmpeg/ffmpeg_ubi_9.6.sh
@@ -307,6 +307,6 @@ cd $WORK_DIR
 python3.12 -m pip install setuptools wheel build
 # Build wheel 
 #python3.12 setup.py bdist_wheel --plat-name=linux_x86_64 --outdir=$WORK_DIR
-python3.12 -m build --wheel --no-isolation --dist-dir=$WORK_DIR
+python3.12 -m build --wheel --no-isolation --outdir=$WORK_DIR
 
 exit 0
