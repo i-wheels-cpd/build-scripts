fbd16bf7e98df2d5baf08d58c8ef1ec550de8bf0
Bharat-Tatti <bharat.tatti@ibm.com>
2025-10-20 17:48:51 +0530

    Updated bucket names

--- process_bom/ca_config.py
+++ process_bom/ca_config.py
@@ -35,8 +35,8 @@ BOM_TOOLS = [
 "Syft",
 "Clair",
 ]
-CLOUD_OBJECT_BUCKET_NAME = "ose-power-toolci-bucket-production"
-CLOUD_OBJECT_CVE_SBOM_BUCKET = "ose-power-sbom-cve-details-production"
+CLOUD_OBJECT_BUCKET_NAME = "ose-x86-toolci-bucket-production"
+CLOUD_OBJECT_CVE_SBOM_BUCKET = "ose-x86-sbom-cve-details-production"
 CLOUD_OBJECT_STORAGE_URL = "https://s3.us.cloud-object-storage.appdomain.cloud"
 CLOUD_OBJECT_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
 
