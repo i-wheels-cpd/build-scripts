8020bd79b8631e51cf58b1883a72731da311a384
Nayana Thorat <nayana.thorat@gmail.com>
2026-09-04 14:34:57 +0530

    Create libprotobuf_ubi_10.2.sh

---
+++ l/libprotobuf/libprotobuf_ubi_10.2.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : libprotobuf
+# Version          : v6.33.6
+# Source repo      : https://github.com/protocolbuffers/protobuf
+# Tested on        : UBI:10.2
+# Language         : Python
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Rushikesh.Sathe1@ibm.com <Rushikesh.Sathe1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+PACKAGE_NAME=libprotobuf
+PACKAGE_VERSION=${1:-v33.6}
+PACKAGE_URL=https://github.com/protocolbuffers/protobuf
+PACKAGE_DIR="protobuf"
+WORK_DIR=$(pwd)
+
+yum install -y make libtool cmake git wget xz zlib-devel openssl-devel bzip2-devel libffi-devel libevent-devel patch python3.14 python3.14-devel python3.14-pip ninja-build gcc-toolset-15
+
+python3.14 -m pip install --upgrade cmake pip setuptools wheel ninja packaging
+
+
+echo "------------ libprotobuf installing-------------------"
+export PATH=/opt/rh/gcc-toolset-15/root/usr/bin:$PATH
+export C_COMPILER=$(which gcc)
+export CXX_COMPILER=$(which g++)
+
+#Build libprotobuf
+git clone $PACKAGE_URL
+cd $PACKAGE_DIR
+git checkout $PACKAGE_VERSION
+
+LIBPROTO_DIR=$(pwd)
+mkdir -p $LIBPROTO_DIR/local/libprotobuf
+LIBPROTO_INSTALL=$LIBPROTO_DIR/local/libprotobuf
+
+git submodule update --init --recursive
+rm -rf ./third_party/googletest | true
+
+mkdir build
+cd build
+
+#Building and testing is performed through the same command
+if ! (cmake -G "Ninja" \
+   ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_C_COMPILER=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+    -DCMAKE_INSTALL_PREFIX=$LIBPROTO_INSTALL \
+    -Dprotobuf_BUILD_TESTS=OFF \
+    -Dprotobuf_BUILD_SHARED_LIBS=ON \
+    -Dprotobuf_ABSL_PROVIDER="module" \
+    -Dprotobuf_JSONCPP_PROVIDER="package" \
+    -Dprotobuf_USE_EXTERNAL_GTEST=OFF \
+    ..) ; then
+    echo "------------------$PACKAGE_NAME:install_&_test_both_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+fi
+
+cmake --build . --verbose
+cmake --install .
+
+cd ..
+wget https://raw.githubusercontent.com/i-wheels-cpd/build-scripts/refs/heads/main/l/libprotobuf/pyproject.toml
+sed -i "s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g" pyproject.toml
+
+python3.14 -m pip wheel -w $WORK_DIR -vv --no-build-isolation --no-deps .
+
+exit 0
